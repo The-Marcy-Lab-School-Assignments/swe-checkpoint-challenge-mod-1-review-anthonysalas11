d750e33d839bc472661102e25a4515c568c7854c
anthonysalas11 <anthony.salas.ts@gmail.com>
2025-12-11 12:13:56 -0500

    progress made

--- src/1-create-slug.js
+++ src/1-create-slug.js
@@ -1,9 +1,16 @@
 // Return a URL-friendly "slug": lowercase with hyphens instead of spaces.
 // Return null if the title contains banned characters: "!", "#", "?"
 const createSlug = (title) => {
+title.toLowercase
+  for(let i = 0; i < title.length;i++){
+  if (title[i] === " ") {
+    return '-'
+   } 
 
+  } 
 };
+console.log(createSlug("hello there"))
+//module.exports = {
+ // createSlug,
+//};
 
-module.exports = {
-  createSlug,
-};
